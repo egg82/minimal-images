7ac1d53cc5c87616fb7a389525a2dc95da19bfb2
egg82 <contact.egg82@pm.me>
2025-07-31 20:41:41 -0600

    Add actualbudget

---
+++ actualbudget/entrypoint.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+if [ -z "${TERM_GRACE_PERIOD}" ]; then
+  TERM_GRACE_PERIOD=10
+fi
+
+_term() {
+  printf "\nCaught SIGTERM, forwarding to app..\n" >&2
+  kill -TERM "$child" 2>/dev/null || true
+
+  ( sleep "$GRACE_PERIOD"
+    if kill -0 "$child" 2>/dev/null; then
+      echo "App didn't exit in ${GRACE_PERIOD}s - force-killing.." >&2
+      kill -KILL "$child" 2>/dev/null || true
+    fi
+  ) &
+}
+_int() {
+  printf "\nCaught SIGINT, forwarding to app..\n" >&2
+  kill -INT "$child" 2>/dev/null || true
+
+  ( sleep "$GRACE_PERIOD"
+    if kill -0 "$child" 2>/dev/null; then
+      echo "App didn't exit in ${GRACE_PERIOD}s - force-killing.." >&2
+      kill -KILL "$child" 2>/dev/null || true
+    fi
+  ) &
+}
+
+trap _term SIGTERM
+trap _int SIGINT
+
+echo "Starting actualbudget.."
+
+node build/app.js &
+child=$!
+
+wait "$child"
+exit $?
---
+++ actualbudget/test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+APP_VERSION=v25.7.1
+
+# docker build --build-arg APP_VERSION=$APP_VERSION -t actualbudget:test-minimal --file Dockerfile-ubi9-minimal .
+# docker run --rm actualbudget:test-minimal
+
+# docker build --build-arg APP_VERSION=$APP_VERSION -t actualbudget:test-micro --file Dockerfile-ubi9-micro .
+# docker run --rm actualbudget:test-micro
+
+docker build --build-arg APP_VERSION=$APP_VERSION -t actualbudget:test-scratch --file Dockerfile-scratch .
+docker run --rm actualbudget:test-scratch
+
+# docker build --build-arg APP_VERSION=$APP_VERSION -t actualbudget:test-alpine --file Dockerfile-alpine .
+# docker run --rm actualbudget:test-alpine
