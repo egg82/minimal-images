a6141aa9b20a44c46042d27a766720da3a2dabe0
egg82 <contact.egg82@pm.me>
2025-08-02 18:53:09 -0600

    Add crond to photoprism containers

--- photoprism/test.sh
+++ photoprism/test.sh
@@ -4,11 +4,11 @@
 
 APP_VERSION=250707-d28b3101e
 
-docker build --build-arg APP_VERSION=$APP_VERSION --build-arg PLATFORM=linux/amd64 -t photoprism:test-minimal --file Dockerfile-ubi9-minimal .
-docker run --rm photoprism:test-minimal
+# docker build --build-arg APP_VERSION=$APP_VERSION --build-arg PLATFORM=linux/amd64 -t photoprism:test-minimal --file Dockerfile-ubi9-minimal .
+# docker run --rm photoprism:test-minimal
 
-docker build --build-arg APP_VERSION=$APP_VERSION --build-arg PLATFORM=linux/amd64 -t photoprism:test-micro --file Dockerfile-ubi9-micro .
-docker run --rm photoprism:test-micro
+# docker build --build-arg APP_VERSION=$APP_VERSION --build-arg PLATFORM=linux/amd64 -t photoprism:test-micro --file Dockerfile-ubi9-micro .
+# docker run --rm photoprism:test-micro
 
 docker build --build-arg APP_VERSION=$APP_VERSION --build-arg PLATFORM=linux/amd64 -t photoprism:test-scratch --file Dockerfile-scratch .
 docker run --rm photoprism:test-scratch
