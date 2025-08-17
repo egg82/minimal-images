abe34eb46b66144e3a8ad81d1ccda3efcb5b00d2
egg82 <contact.egg82@pm.me>
2025-08-17 14:02:23 -0600

    Add searxng

---
+++ searxng/entrypoint.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+TERM_GRACE_PERIOD=${TERM_GRACE_PERIOD:-10}
+
+export SEARXNG_BIND_ADDRESS=${SEARXNG_BIND_ADDRESS-0.0.0.0}
+export SEARXNG_PORT=${SEARXNG_PORT-8080}
+
+_term() {
+  printf "\nCaught SIGTERM, forwarding to app..\n" >&2
+  kill -TERM "$child" 2>/dev/null || true
+
+  ( sleep "$TERM_GRACE_PERIOD"
+    if kill -0 "$child" 2>/dev/null; then
+      echo "App didn't exit in ${TERM_GRACE_PERIOD}s - force-killing.." >&2
+      kill -KILL "$child" 2>/dev/null || true
+    fi
+  ) &
+}
+_int() {
+  printf "\nCaught SIGINT, forwarding to app..\n" >&2
+  kill -INT "$child" 2>/dev/null || true
+
+  ( sleep "$TERM_GRACE_PERIOD"
+    if kill -0 "$child" 2>/dev/null; then
+      echo "App didn't exit in ${TERM_GRACE_PERIOD}s - force-killing.." >&2
+      kill -KILL "$child" 2>/dev/null || true
+    fi
+  ) &
+}
+
+trap _term SIGTERM
+trap _int SIGINT
+
+echo "Starting searxng.."
+
+/app/webapp "$@" &
+child=$!
+
+wait "$child"
+exit $?
---
+++ searxng/test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# https://github.com/searxng/searxng/blob/master/container/builder.dockerfile
+
+APP_VERSION=6b1516d
+
+VOLUMES=( config data )
+
+for vol in "${VOLUMES[@]}"; do
+  docker volume create "$vol"
+  docker run --rm -v "$vol":/data alpine:latest sh -c "chown -R 1000:1000 /data"
+done
+
+docker build --build-arg APP_VERSION=$APP_VERSION -t searxng:test-minimal --file Dockerfile-ubi9-minimal .
+docker run -p 127.0.0.1:8080:8080 -v config:/etc/searxng -v data:/var/cache/searxng -e SEARXNG_SECRET="$(openssl rand -hex 32)" --rm searxng:test-minimal
+
+docker build --build-arg APP_VERSION=$APP_VERSION -t searxng:test-micro --file Dockerfile-ubi9-micro .
+docker run -p 127.0.0.1:8080:8080 -v config:/etc/searxng -v data:/var/cache/searxng -e SEARXNG_SECRET="$(openssl rand -hex 32)" --rm searxng:test-micro
+
+docker build --build-arg APP_VERSION=$APP_VERSION -t searxng:test-scratch --file Dockerfile-scratch .
+docker run -p 127.0.0.1:8080:8080 -v config:/etc/searxng -v data:/var/cache/searxng -e SEARXNG_SECRET="$(openssl rand -hex 32)" --rm searxng:test-scratch
+
+docker build --build-arg APP_VERSION=$APP_VERSION -t searxng:test-alpine --file Dockerfile-alpine .
+docker run -p 127.0.0.1:8080:8080 -v config:/etc/searxng -v data:/var/cache/searxng -e SEARXNG_SECRET="$(openssl rand -hex 32)" --rm searxng:test-alpine
+
+for vol in "${VOLUMES[@]}"; do
+  docker volume rm "$vol"
+done
